25a05d9b11ffbaddf892f8cc526ab6a5220da1b4
Ricardo Ferraz <ricardo@mgsystems.com.br>
2014-06-26 09:53:02 -0500

    Changes made by Ricardo Ferraz (ricardo@mgsystems.com.br).

---
+++ walllet/scripts/wallets.js
@@ -0,0 +1,35 @@
+(function (global) {
+    var WalletsViewModel,
+        app = global.app = global.app || {};
+
+    WalletsViewModel = kendo.data.ObservableObject.extend({
+        walletsDataSource: null,
+
+        init: function () {
+            var that = this,
+                dataSource,
+                jsonUrlToLoad;
+
+            kendo.data.ObservableObject.fn.init.apply(that, []);
+
+            //When you build for Apache Cordova 3.0.0, apply this code instead of using relative URLs. In Apache Cordova 3.0.0, relative URLs might not work properly.
+            //jsonUrlToLoad = app.makeUrlAbsolute("data/weather.json");
+            jsonUrlToLoad = "data/wallets.json";
+
+            dataSource = new kendo.data.DataSource({
+                transport: {
+                    read: {
+                        url: jsonUrlToLoad,
+                        dataType: "json"
+                    }
+                }
+            });
+
+            that.set("walletsDataSource", dataSource);
+        }
+    });
+
+    app.walletsDataSource = {
+        viewModel: new WalletsViewModel()
+    };
+})(window);
