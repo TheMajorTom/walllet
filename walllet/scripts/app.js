2c52e14ec6f7d7fe762b24d0a01ac2123ad1298c
Ricardo Ferraz <ricardo@mgsystems.com.br>
2014-06-27 10:30:38 -0500

    Changes made by Ricardo Ferraz (ricardo@mgsystems.com.br).

--- walllet/scripts/app.js
+++ walllet/scripts/app.js
@@ -1,7 +1,8 @@
 (function (global) 
 {
-    var app = global.app = global.app || {};
 
+    var app = global.app = global.app || {};
+	    
     app.makeUrlAbsolute = function (url)
     {
             var anchorEl = document.createElement("a");
@@ -31,8 +32,9 @@
 
             app.application.skin(mobileSkin);
         };
-
+        
         app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout", hideAddressBar: true });
+        
         
     }, false);
     
